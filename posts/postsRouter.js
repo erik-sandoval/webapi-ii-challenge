d638da2758d60cf395b55e534b8f9539a72cb413
erik-sandoval <erik.sandoval@icloud.com>
2019-05-28 23:14:09 -0700

    get and add comment set up

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -29,3 +29,37 @@ router.post("/", (req, res) => {
     res.status(401).json({ message: "title and content required for a post." });
   }
 });
+
+// create a comment under a post id or view comments of a post id
+router.get("/:id/comments", (req, res) => {
+  const id = req.params.id;
+  Posts.findCommentById(id)
+    .then(data => {
+      console.log(data.length > 0);
+      if (data.length > 0) {
+        console.log(data);
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({ message: "comment does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "could not get comment." });
+    });
+});
+
+router.post("/:id/comments", (req, res) => {
+  const id = req.params.id;
+  const comment = req.body;
+  if (comment.text) {
+    Posts.insertComment(comment)
+      .then(data => {
+        res.status(201).json(data);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "could not add comment." });
+      });
+  } else {
+    res.status(401).json({ message: "please input text." });
+  }
+});
